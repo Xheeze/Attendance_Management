931d98739ae5fbb90e516023b35b6c274346b58e
Ntokozo Sibiya <109238828+Xheeze@users.noreply.github.com>
2025-10-29 00:33:41 +0200

    adding dependancies

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Attendance Management/',
+  base: '/Attendance_Management/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
